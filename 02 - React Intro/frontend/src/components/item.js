5d6ce92ceb20664ac1cde6ea7804422ac6f1ef7e
Uptaker <markus.tammeoja@gmail.com>
2021-09-06 11:04:55 +0300

    Add component

---
+++ 02 - React Intro/frontend/src/components/item.js
@@ -0,0 +1,11 @@
+function Item() {
+    return (
+        <div>
+            <div className="itemName">Item #1</div>
+            <div className="itemName">100</div>
+            <div className="itemName">phones</div>
+        </div>
+    );
+}
+
+export default Item;

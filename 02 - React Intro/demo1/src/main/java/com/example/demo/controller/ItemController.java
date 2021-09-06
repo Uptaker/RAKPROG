0310d3d8e07987ccdcf2385e16fe4674f7d90117
Uptaker <markus.tammeoja@gmail.com>
2021-09-06 09:46:14 +0300

    Services, repositories, controllers

--- 02 - React Intro/demo1/src/main/java/com/example/demo/controller/ItemController.java
+++ 02 - React Intro/demo1/src/main/java/com/example/demo/controller/ItemController.java
@@ -1,20 +1,49 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Item;
+import com.example.demo.repository.ItemRepository;
+import com.example.demo.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ItemController {
 
+    ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping("items")
-    public String getItems() {
-        return "works";
+    public List<Item> getItems() {
+        return itemService.getItems();
     }
 
     @PostMapping("items")
-    public String postItem(@RequestBody String string) {
-        return "posted: " + string;
+    public String postItem(@RequestBody Item item) {
+        itemService.saveItem(item);
+        /*
+        In Postman:
+        {
+            "id": 1,
+                "name": "aaa",
+                "price": 10,
+                "category": "wee"
+
+        }
+         */
+        return "posted: " + item.getName();
     }
+
+    // delete päring
+    // edit päring
+    // view one item
+
+    // kategooria osa
+
 }

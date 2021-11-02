75f45e573512d5ab544ffc0d89d8669e9dc00061
Uptaker <markus.tammeoja@gmail.com>
2021-11-03 01:42:51 +0200

    fix to correctly update via body, refactoring

--- 03 - React Intro 2/backend-node/src/controllers/post.js
+++ 03 - React Intro 2/backend-node/src/controllers/post.js
@@ -2,47 +2,32 @@ const Post = require('../models/Post')
 
 exports.getPosts = async (req, res) => {
   const posts = await Post.find({})
-  
   res.status(200).send(posts)
 }
 
 exports.createPost = async (req, res) => {
-
-    const {title, text, user} = req.body;
-
+  const {title, text, user} = req.body;
   const newPost = {
     title,
     text,
     user
   }
-
   const createdPost = new Post(newPost)
-
   const savedPost = await createdPost.save()
-
   res.status(200).send(`yay ${savedPost._id}`)
 }
 
 exports.updatePost = async (req, res) => {
-
-    //for only updating the value
-    const {id} = req.params;
-
-    const Post = await Post.findOne({_id: id});
-
-    if (!Post) res.status(404).send("No post with that ID")
-
-    const updated = await Post.findOneAndUpdate({_id: id, }, {title: item.title + " EDITED"}, {new: true}) // +1 to quality
-
-    res.status(200).send(`Successfullyy updated. \n BEFORE:\n ${item}\n AFTER:\n ${updated}`)
+  const {id} = req.params;
+  const Post = await Post.findOne({_id: id});
+  if (!Post) res.status(404).send("No post with that ID")
+  const updated = await Post.findOneAndUpdate({_id: id, }, req.body, {new: true})
+  res.status(200).send(`Successfullyy updated. \n BEFORE:\n ${item}\n AFTER:\n ${updated}`)
 }
 
 exports.deletePost = async (req, res) => {
   const { id } = req.params;
-
-  const item = await Post.findOneAndDelete({ _id: id })
-
-  if (!post) res.status(404).send("No post with that id found")
-
+  const post = await Post.findOneAndDelete({ _id: id })
+  if (!post) res.status(404).send("No post with id " + id + " found")
   res.status(200).send(`Successfully deleted the following post: \n ${post}`)
 }

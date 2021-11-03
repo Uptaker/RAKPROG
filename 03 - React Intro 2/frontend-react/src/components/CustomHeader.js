993f62a459c2bbdfefee4d4f344f5e31f364af2a
Uptaker <markus.tammeoja@gmail.com>
2021-11-03 03:10:30 +0200

    add login/logout functionality with JWT

--- 03 - React Intro 2/frontend-react/src/components/CustomHeader.js
+++ 03 - React Intro 2/frontend-react/src/components/CustomHeader.js
@@ -2,10 +2,18 @@ import React from 'react'
 // import {Button, Layout} from 'antd'
 import {Link} from 'react-router-dom'
 import '../scss/Header.scss';
-
-
+import { Context } from '../store';
+import {useContext} from 'react'
+import { logoutUser } from '../store/actions';
 
 function CustomHeader() {
+
+    const [state, dispatch] = useContext(Context)
+
+    function handleLogout() {
+        dispatch(logoutUser())
+    }
+
     return (
         <nav>
             <div className="div-left">
@@ -13,10 +21,18 @@ function CustomHeader() {
                 <Link to="/posts">Posts</Link>
             </div>
 
+            {!state.auth.token ?
             <div className="div-right">
                 <Link to="/register">Register   </Link>
                 <Link to="/login">Login</Link>
             </div>
+            :
+            <div className="div-right">
+                <Link to="#">Welcome, {state.auth.user.firstName}</Link>
+                <Link onClick={handleLogout} to="#">Log out</Link>
+            </div>
+            }
+
         </nav>
     )
 }
